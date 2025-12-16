4d8f8de5d9bf4b2738ca9ccc3759a04646e0e633
Abhidb2007 <abhishekdb2210@gmail.com>
2025-12-16 23:33:21 +0530

    code add

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,7 +2,7 @@ import {webSocketServer} from "ws";
 const wss=new webSocketServer({port:8080});
 wss.on("connection", function connection(ws){
     ws.on("message", function message(data){
-        ws.sed("ping");
+        ws.sed("abhi");
     })
 })
 
