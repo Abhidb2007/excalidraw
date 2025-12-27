0e7e7e6d060a573846b1d4de2ba9e03092d5873b
Abhidb2007 <abhishekdb2210@gmail.com>
2025-12-27 11:40:22 +0530

    add code

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,29 @@
 import express from "express";
-const app=express();
-app.post("/signup",(req, res)=>{
+import jwt from "jsonwebtoken";
+import{JWT_SECRET} from "./config";
+import { middleware } from "./middleware";
 
-})
-app.post("/signin",(req, res)=>{
-}); 
-app. post("/room",(req, res)=>{
-});  
 
-app.listen(3000);
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup", (req, res) => {
+	
+});
+
+app.post("/signin", (req, res) => {
+    const userId=1;
+    jwt.sign({
+        userId
+    },JWT_SECRET);
+	
+});
+
+app.post("/room", middleware, (req, res) => {
+	
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on http://localhost:3000");
+});
