f8374a2779cc98d8147d9106578d8457b92c2e65
Abhidb2007 <abhishekdb2210@gmail.com>
2025-12-27 19:27:19 +0530

    add ws-backend code

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,40 @@
-
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
+
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
-    ws.on("message", function message(data) {
-        if (data.toString() === "ping") {
-            ws.send("pong");
-        }
-    });
+wss.on("connection", (ws, req) => {
+  const url = req.url; // example: ws://localhost:8080?token=123123
+
+  if (!url) {
+    ws.close();
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token");
+
+  if (!token) {
+    ws.close();
+    return;
+  }
+
+  let decoded: JwtPayload;
+
+  try {
+    decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+  } catch (err) {
+    ws.close();
+    return;
+  }
+
+  if (!decoded.userId) {
+    ws.close();
+    return;
+  }
+
+  ws.on("message", () => {
+    ws.send("pong");
+  });
 });
